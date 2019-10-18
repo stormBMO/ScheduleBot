18afb22059958a1e018faab1bcc392cb17e2d7ac
stormBMO <andrey.chet76@gmail.com>
2019-10-18 22:13:55 +0300

    Заработалл

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import telebot;
-bot = telebot.TeleBot('%897488154:AAHM8Ghj65Xj6BP_fC8C6CWL-ZF7cs20FOA%');
+import telebot
+import requests
+#--------------------------------------------------------------------
+token_bot = '897488154:AAHM8Ghj65Xj6BP_fC8C6CWL-ZF7cs20FOA'
+#--------------------------------------------------------------------
+
+bot = telebot.TeleBot(token_bot)
 
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
+     print(message.text)
      if message.text == "Привет":
-          bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+          bot.send_message(message.from_user.id, "Привет, ты гей??")
      elif message.text == "/help":
           bot.send_message(message.from_user.id, "Напиши привет")
      else:
           bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 bot.polling(none_stop=True, interval=0)
+
