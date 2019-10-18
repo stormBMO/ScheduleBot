87a1d3132d646939cf47ad25aeac9ac209a39e7b
stormBMO <andrey.chet76@gmail.com>
2019-10-18 22:57:02 +0300

    Изменил взаимодействие с пользователем

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import telebot
 import requests
 #--------------------------------------------------------------------
 token_bot = '897488154:AAHM8Ghj65Xj6BP_fC8C6CWL-ZF7cs20FOA'
+name = ''
+
 #--------------------------------------------------------------------
 
 bot = telebot.TeleBot(token_bot)
@@ -9,13 +11,10 @@ bot = telebot.TeleBot(token_bot)
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
-     print(message.text)
-     if message.text == "Привет":
-          bot.send_message(message.from_user.id, "Привет, хочешь добавить создать хз надо подумать") 
-     elif message.text == "/help":
-          bot.send_message(message.from_user.id, "Напиши привет") #надо бы продумать логику тут (ответа)
+     print(message.from_user.username + " отправил: " + message.text)
+     if message.text == "/help":
+          bot.send_message(message.from_user.id, "Напиши /reg, чтобы зарегестрироваться") #надо бы продумать логику тут (ответа)
      else:
           bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 bot.polling(none_stop=True, interval=0)
-
