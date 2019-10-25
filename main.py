d0d859478071ed7a7c5287b5c64ce2d92ad194e8
stormBMO <andrey.chet76@gmail.com>
2019-10-25 14:48:22 +0300

    Добавил пострег

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ bot = telebot.TeleBot(token_bot)
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
-     print(message.from_user.username + " отправил: " + message.text)
-     if message.text == "/help":
-          bot.send_message(message.from_user.id, "Этот бот поможет тебе с расписанием в университете. Напиши /reg, чтобы зарегистрироваться") #надо бы продумать логику тут (ответа)
-     elif message.text == "/reg":
-          bot.send_message(message.from_user.id, "Спасибо, что решил попробовать нашего бота <3")
-          bot.send_sticker(message.from_user.id, 'CAADAgADJAADO2AkFPvZAoRAR-UBFgQ')
-          bot.send_message(message.from_user.id, "Как тебя зовут?")
-          bot.register_next_step_handler(message, get_name)
+     print(message.from_user.username + " отправил: " + message.text) 
+     if name == '':
+          if message.text == "/help":
+               bot.send_message(message.from_user.id, "Этот бот поможет тебе с расписанием в университете. Напиши /reg, чтобы зарегистрироваться") #надо бы продумать логику тут (ответа)
+          elif message.text == "/reg":
+               bot.send_message(message.from_user.id, "Спасибо, что решил попробовать нашего бота <3")
+               bot.send_sticker(message.from_user.id, 'CAADAgADJAADO2AkFPvZAoRAR-UBFgQ')
+               bot.send_message(message.from_user.id, "Как тебя зовут?")
+               bot.register_next_step_handler(message, get_name)
+          else:
+               bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
      else:
-          bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+               if message.text == "/whoami":
+                    bot.send_message(message.from_user.id, 'Ты - ' + name + ', учишься на факультете ' + faculty);
+               else:
+                    bot.send_message(message.from_user.id, 'Не знаю что ты сказал, но я пока только понимаю команды: /whoami');
+
 
 
 def get_name(message):
